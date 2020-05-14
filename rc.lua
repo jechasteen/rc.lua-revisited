a18b6c21bc4fabbcf7a743dce4576fa4ef86b5a2
Jonathan Chasteen <jonathan.chasteen@live.com>
2020-05-14 01:06:25 -0400

    Corrections to minor issues with formatting

--- rc.lua
+++ rc.lua
@@ -94,15 +94,15 @@ myawesomemenu = {
 }
 
 mymainmenu = awful.menu({
-items = {
-    { "awesome", myawesomemenu, beautiful.awesome_icon },
-    { "open terminal", terminal }
-}
+    items = {
+        { "awesome", myawesomemenu, beautiful.awesome_icon },
+        { "open terminal", terminal }
+    }
 })
 
 mylauncher = awful.widget.launcher({
-image = beautiful.awesome_icon,
-menu = mymainmenu
+    image = beautiful.awesome_icon,
+    menu = mymainmenu
 })
 
 menubar.utils.terminal = terminal -- Set the terminal for applications that require it
